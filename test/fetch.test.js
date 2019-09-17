75bb24d5ab89f5890960f65f0ad835b96462fb1a
develowlper <dev@develowlper.de>
2019-09-17 16:11:11 +0200

    added basic jest test

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -1,6 +1,13 @@
-import { fetchJson5 } from '../src';
+import { fetchJson5, tryFetchJson5 } from '../src';
+import JSON5 from 'json5';
 
 test('fetch json', async () => {
-  const res = await fetchJson5('test.json5');
-  console.log('TCL: res', res);
+  fetch.mockResponseOnce(JSON5.stringify({ test: 'test' }));
+  await fetchJson5('test.json5');
+});
+
+test('try fetch json', async () => {
+  fetch.mockResponseOnce('HELLO');
+  const res = await tryFetchJson5('test.json5');
+  expect(res).toEqual({});
 });
