24517413c4fdbe98f928e0dd2c3468297900aa05
develowlper <dev@develowlper.de>
2021-06-10 13:40:44 +0200

    fix: update dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-export * from './fetch';
+import JSON5 from 'json5';
+
+const fetchJson5 = async path => {
+  return JSON5.parse(await (await fetch(path)).text());
+};
+
+const tryFetchJson5 = async path => {
+  try {
+    return JSON5.parse(await (await fetch(path)).text());
+  } catch (e) {
+    console.warn(`Fetching ${path} caused the following error:`, e);
+  }
+  return null;
+};
+
+export { tryFetchJson5, fetchJson5 };
